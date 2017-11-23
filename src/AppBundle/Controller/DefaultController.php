5e418d9f512929d0a818326ac70412496007028f
Theodor Manolescu <thedor.manolescu@emag.ro>
2017-11-23 22:47:49 +0200

    test commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -21,7 +21,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        // replace this example code with whatever you need
+        // replace this example code with whatever syou need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
